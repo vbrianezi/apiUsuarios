60254aab386e3f2a522d671f73918999943be9bd
Viviane Brianezi <vbrianezi@hotmail.com>
2022-08-20 04:28:27 -0300

    x

--- CadastroDeUsuarios.API/Startup.cs
+++ CadastroDeUsuarios.API/Startup.cs
@@ -46,7 +46,6 @@ namespace CadastroDeUsuarios.API
             });
             
             services.AddScoped<IUsuariosRepositorio, UsuariosRepositorio>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
